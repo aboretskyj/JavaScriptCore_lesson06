5641f68c18827a2c335a8e24e20339ff6094281f
aboretskyj <37591837+aboretskyj@users.noreply.github.com>
2019-06-15 21:08:48 +0300

    Update task_02.js
    The formatting of code was corrected.

--- task_02.js
+++ task_02.js
@@ -3,11 +3,12 @@
 // We don't know the number of the input parameters
 
 function min() {
-
     var m = arguments[0];
+    
     for (var i = 0; i < arguments.length; i++) {
         if (m > arguments[i]) 
         m = arguments[i];
     }
+    
     return m;
 }
